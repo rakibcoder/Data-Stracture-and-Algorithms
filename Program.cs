811e95aede811e9f603cfe3e1cdb33ae4b5ba0f8
Rakib <rakibul674@gmail.com>
2024-02-23 16:54:28 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DataStractureAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3 };
+            int index= ints.LinearSearch(2);
+
+            Console.WriteLine(index);
+        }
+    }
+}
---
+++ Search.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStractureAlgorithms
+{
+    internal static class Search
+    {
+        /// <summary>
+        /// Get Search Value Index number from Array By Using Linear Search Algorithms
+        /// </summary>
+        /// <param name="intArr">input integer array</param>
+        /// <param name="searchValue">which value search into array</param>
+        /// <returns>if searchValue is found then return searchValue index number
+        /// or return -1</returns>
+        /// <exception cref="ArgumentNullException">Array is null</exception>
+        public static int LinearSearch(this int[] intArr,int searchValue)
+        {
+            int i;
+
+            if (intArr==null)
+            {
+                throw new ArgumentNullException(nameof(intArr));
+            }
+            
+            for (i=0;i<intArr.Length;i++)
+            {
+                if (intArr[i] == searchValue)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
