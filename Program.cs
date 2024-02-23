6c72ef8f975d7b3d259304ec6ed734d0338d13b7
Rakib <rakibul674@gmail.com>
2024-02-23 16:57:45 +0900

    LinearSearch Method variable name change i replace to indexcomment correction

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@
     {
         static void Main(string[] args)
         {
-            int[] ints = { 1, 2, 3 };
-            int index= ints.LinearSearch(2);
+            int[] IntArray = { 1, 2, 3 ,50,90};
+
+            
+            int index= IntArray.LinearSearch(4);
 
             Console.WriteLine(index);
         }
--- Search.cs
+++ Search.cs
@@ -9,7 +9,7 @@ namespace DataStractureAlgorithms
     internal static class Search
     {
         /// <summary>
-        /// Get Search Value Index number from Array By Using Linear Search Algorithms
+        /// Get Search Value Indexnumber from Array By Using Linear Search Algorithms
         /// </summary>
         /// <param name="intArr">input integer array</param>
         /// <param name="searchValue">which value search into array</param>
@@ -18,20 +18,21 @@ namespace DataStractureAlgorithms
         /// <exception cref="ArgumentNullException">Array is null</exception>
         public static int LinearSearch(this int[] intArr,int searchValue)
         {
-            int i;
+            int index;
 
             if (intArr==null)
             {
                 throw new ArgumentNullException(nameof(intArr));
             }
             
-            for (i=0;i<intArr.Length;i++)
+            for (index=0;index<intArr.Length;index++)
             {
-                if (intArr[i] == searchValue)
+                if (intArr[index] == searchValue)
                 {
-                    return i;
+                    return index;
                 }
             }
+
             return -1;
         }
     }
