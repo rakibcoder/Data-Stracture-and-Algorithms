b01e9ccdb05913673ad095ffcab255c45025c231
Rakib Hasan Mozumder <rakibul674@gmail.com.com>
2024-03-12 15:54:26 +0900

    add selection sort algorithom

--- Program.cs
+++ Program.cs
@@ -8,13 +8,19 @@ namespace DataStractureAlgorithms
         {
             //Linear Search
             int[] intArray = { 1, 2, 3, 50, 90 };
-            int index= intArray.LinearSearch(4);
+            int index= intArray.LinearSearch(50);
             PrintResult("LinearSearch", index);
 
             //Binary Search
             int[] intSortArray = { 1, 2, 3, 50, 90,100,110};
             index = intSortArray.BinarySearch(90);
             PrintResult("BinarySearch", index);
+
+            //SelectionSort
+            int[] intUnsortArr = { 8, 50, 0, -12, 56, 2, 67, 985, 3 };
+            //PrintAllValue("Before sort", intUnsortArr);
+            intUnsortArr.SelectionSort();
+            PrintAllValue("SelectionSort", intUnsortArr);
 
         }
 
@@ -29,6 +35,17 @@ namespace DataStractureAlgorithms
             {
                 Console.WriteLine($"{methodName} input Value Index number is {result}");
             }
+        }
+
+        static void PrintAllValue(string methodName, int[] intArr)
+        {
+            string resultString = $"{methodName}: ";
+
+            foreach (var item in intArr)
+            {
+                resultString += $"{item}\t";
+            }
+            Console.WriteLine(resultString);
         }
     }
 }
---
+++ Sort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStractureAlgorithms
+{
+    internal static class Sort
+    {
+        /// <summary>
+        /// Sort int array by using Selection sort Algorithom
+        /// </summary>
+        /// <param name="intArr"></param>
+        public static void SelectionSort(this int[] intArr)
+        {
+            if (intArr == null)
+            {
+                throw new ArgumentNullException(nameof(intArr));
+            }
+
+            // Outer loop: iterate through each element in the array
+            //n-1: not need to sort last element,because last element already sort
+            for (int i = 0; i < intArr.Length - 1; i++)
+            {
+                // Assume the current element is the minimum
+                int minValue = intArr[i];
+                int minIndex = i;
+
+                // Inner loop: find the minimum element in the remaining unsorted part
+                for (int j = i + 1; j < intArr.Length; j++)
+                {
+                    if (minValue > intArr[j])
+                    {
+                        // Update the minimum value and index if a smaller element is found
+                        minValue = intArr[j];
+                        minIndex = j;
+                    }
+                }
+
+                // Swap the minimum element with the first element in the unsorted part
+                if (minIndex != i)
+                {
+                    intArr[minIndex] = intArr[i];
+                    intArr[i] = minValue;
+                }
+            }
+        }
+    }
+}
